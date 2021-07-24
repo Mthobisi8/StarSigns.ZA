1b1f9aa7618adc1ddff6e65c360e7bfdcaa1ca2a
Mthobisi8 <mthobisimagubane8@gmail.com>
2021-07-24 12:48:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+Function {
+   var fullDate = new Date();
+   var hour = fullDate.getHours();
+   var mins = fullDate.getMinutes();
+   var sec = fullDate.getSeconds();
+
+
+   document.getElementById('hour').innerHTML = hour
+
+
+  
